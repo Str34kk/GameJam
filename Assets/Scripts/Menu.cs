aeece5c474eaeb97af3488e11bd953eb152bd6dd
Kamil Klecha <urlikodd@gmail.com>
2020-12-28 16:14:23 +0100

    Added simple menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{ 
+    public void btnPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void btnCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void btnExit()
+    {
+        Application.Quit();
+    }
+}
