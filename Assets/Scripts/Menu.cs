3938ac92c480f995ee1626e0d0834a321fcd730b
Kamil Klecha <urlikodd@gmail.com>
2020-12-30 19:59:14 +0100

    Added level select lock

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Menu : MonoBehaviour
-{ 
+{
+    public GridLayoutGroup LevelSelector;
+    void Start()
+    {
+        foreach(Button b in LevelSelector.GetComponentsInChildren<Button>())
+        {
+            string level = b.GetComponentInChildren<TextMeshProUGUI>().text;
+            Image im = b.transform.Find("Lock").GetComponent<Image>();
+
+            if (PlayerPrefs.HasKey(level))
+            {
+                if (PlayerPrefs.GetInt(level) == 1)
+                {
+                    b.interactable = true;
+                    im.gameObject.SetActive(false);
+                }
+                else
+                {
+                    b.interactable = false;
+                    im.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                b.interactable = false;
+                im.gameObject.SetActive(true);
+            }
+        }
+    }
     public void btnPlay()
     {
         SceneManager.LoadScene("Demo");
